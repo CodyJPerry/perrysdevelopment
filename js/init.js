8516ae8d5521ced5bafd0c56fdb5c1361f1f3248
Cperry <codyperry65@yahoo.com>
2017-04-09 14:35:40 -0400

    Optimize for performance

--- js/init.js
+++ js/init.js
@@ -1,9 +1,19 @@
 (function($){
   $(function(){
-
     $('.button-collapse').sideNav();
-    // carousel
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+    // Testmonial Slider
+    $('.slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: true,
+      speed: 600,
+      mobileFirst: true, //Mobile settings
+      swipe: true,
+      swipeToSlide: true
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
